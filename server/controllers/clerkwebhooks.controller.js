737830ba443fe42e292b661405a45b6b07936591
Mhmoud Alnagdy <nagdista@gmail.com>
2025-08-10 19:13:57 +0300

    adddd

--- server/controllers/clerkwebhooks.controller.js
+++ server/controllers/clerkwebhooks.controller.js
@@ -19,9 +19,9 @@ const clerkWebhooks = async (req, res) => {
     // !
     const userData = {
       _id: data.id,
-      email: data.email_adresses[0].email_adress,
+      email: data.email_addresses[0].email_address,
       username: data.first_name + " " + data.last_name,
-      image: data.img_url,
+      image: data.image_url,
     };
 
     // ! Switch Case For Different Event
--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-import express from 'express';
-import 'dotenv/config';
-import connectDB from './configs/db.js';
-await connectDB() ; 
-import { clerkMiddleware } from '@clerk/express'
-import clerkWebhooks from './controllers/clerkwebhooks.controller.js';
+import { clerkMiddleware } from "@clerk/express";
+import "dotenv/config";
+import express from "express";
+import connectDB from "./configs/db.js";
+import clerkWebhooks from "./controllers/clerkwebhooks.controller.js";
+await connectDB();
 
 // ! VARIABLES
 const app = express();
 const port = process.env.PORT || 5001;
 // ! MIDDLEWARE
-app.use(express.json())
-app.use(clerkMiddleware())
+app.use(express.json());
+app.use(clerkMiddleware());
 // ! ROUTE
-app.use('/api/clerk',clerkWebhooks)
-app.get('/',(req,res)=>res.send("API"))
+app.use("/api/clerk", clerkWebhooks);
+app.get("/", (req, res) => res.send("API"));
 
 // ! SERVER
-app.listen(port,()=>{
-  console.log(`Server Starting on port :${port}, http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server Starting on port :${port}, http://localhost:${port}`);
+});
