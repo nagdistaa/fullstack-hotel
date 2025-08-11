a875453efb27c47a26c9e6d2802a5441382b6383
Mhmoud Alnagdy <nagdista@gmail.com>
2025-08-11 14:00:07 +0300

    rewrite

--- server/controllers/clerkwebhooks.controller.js
+++ server/controllers/clerkwebhooks.controller.js
@@ -4,19 +4,16 @@ import User from "../models/User.model.js";
 const clerkWebhooks = async (req, res) => {
   try {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    // ! Getting Header
     const headers = {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     };
 
-    // ! Verify Headers
     await whook.verify(JSON.stringify(req.body), headers);
 
-    // ! Getting Data From REQ Body ;
     const { data, type } = req.body;
-    // !
+
     const userData = {
       _id: data.id,
       email: data.email_addresses[0].email_address,
@@ -24,11 +21,9 @@ const clerkWebhooks = async (req, res) => {
       image: data.image_url,
     };
 
-    // ! Switch Case For Different Event
-
     switch (type) {
       case "user.created": {
-        await User.create(userData);
+        User.create(userData);
         break;
       }
       case "user.updated": {
@@ -39,10 +34,12 @@ const clerkWebhooks = async (req, res) => {
         await User.findByIdAndDelete(data.id);
         break;
       }
+
       default:
         break;
     }
-    res.json({ sucess: true, message: "webhook recieved" });
+
+    res.json({ sucess: true, message: "WebHook Recieved" });
   } catch (error) {
     console.log(error.message);
     res.json({ sucess: false, message: error.message });
